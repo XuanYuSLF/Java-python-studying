2285b77f878f70e4f467f7f95efc56be30ad5aba
python studying <xuanyuslf@gmail.com>
2021-02-19 22:49:41 +0800

    Add java

---
+++ java/abab.java
@@ -0,0 +1,8 @@
+public class abab{
+    public static void main(String[] args) {
+        System.out.println("阿巴阿巴");
+        System.out.println("阿巴阿巴");
+        System.out.println("阿巴阿巴");
+        System.out.println("阿巴阿巴");
+    }
+}
